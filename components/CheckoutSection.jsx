fa025477ce1b4a0b47e344fc2a13b096de913bf1
HittuDesai <hitarth.desai@mail.utoronto.ca>
2022-07-04 11:24:29 +0530

    Initial commit for address input and google places api

--- components/CheckoutSection.jsx
+++ components/CheckoutSection.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import Cart from "./Cart";
 
 import Accordion from '@mui/material/Accordion';
@@ -19,6 +19,11 @@ import { db } from "../firebase"
 import { addDoc, collection, serverTimestamp } from "firebase/firestore"
 
 function CheckoutSection() {
+    const addressInputField = useRef(null);
+    useEffect(() => {
+
+    }, [addressInputField])
+
     const [checkoutCart, setCheckoutCart] = useState({});
     const [amountPayable, setAmountPayable] = useState(0);
     useEffect(() => {
@@ -136,6 +141,16 @@ function CheckoutSection() {
                                     <Typography fontSize="small">WhatsApp is working and preferable</Typography>
                                     <Checkbox color="success" defaultChecked value={whatsAppPreferable} onChange={() => setWhatsAppPreferable(!whatsAppPreferable)} />
                                 </Grid>
+                                <TextField
+                                required
+                                ref={addressInputField}
+                                // onChange={event => handlePhoneNumber(event)} 
+                                variant="filled" 
+                                sx={{ width: "300px", marginTop: "1rem" }} 
+                                // helperText={phoneNumber.length === 10 ? "Done" : `${10 - phoneNumber.length} digits more`}
+                                placeholder="Enter Address"
+                                // value={phoneNumber}
+                                />
                                 <LoadingButton loading={loading} variant="contained" color="info" type="submit" sx={{ width: "300px", marginTop: "1rem" }}>Place Order</LoadingButton>
                             </Paper>
                         </Grid>
@@ -147,6 +162,115 @@ function CheckoutSection() {
 }
 
 export default CheckoutSection;
+
+/*
+
+  <script async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"></script>
+
+// This sample uses the Places Autocomplete widget to:
+// 1. Help the user select a place
+// 2. Retrieve the address components associated with that place
+// 3. Populate the form fields with those address components.
+// This sample requires the Places library, Maps JavaScript API.
+// Include the libraries=places parameter when you first load the API.
+// For example: <script
+// src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+let autocomplete: google.maps.places.Autocomplete;
+let address1Field: HTMLInputElement;
+let address2Field: HTMLInputElement;
+let postalField: HTMLInputElement;
+
+function initAutocomplete() {
+  address1Field = document.querySelector("#ship-address") as HTMLInputElement;
+  address2Field = document.querySelector("#address2") as HTMLInputElement;
+  postalField = document.querySelector("#postcode") as HTMLInputElement;
+
+  // Create the autocomplete object, restricting the search predictions to
+  // addresses in the US and Canada.
+  autocomplete = new google.maps.places.Autocomplete(address1Field, {
+    componentRestrictions: { country: ["us", "ca"] },
+    fields: ["address_components", "geometry"],
+    types: ["address"],
+  });
+  address1Field.focus();
+
+  // When the user selects an address from the drop-down, populate the
+  // address fields in the form.
+  autocomplete.addListener("place_changed", fillInAddress);
+}
+
+function fillInAddress() {
+  // Get the place details from the autocomplete object.
+  const place = autocomplete.getPlace();
+  let address1 = "";
+  let postcode = "";
+
+  // Get each component of the address from the place details,
+  // and then fill-in the corresponding field on the form.
+  // place.address_components are google.maps.GeocoderAddressComponent objects
+  // which are documented at http://goo.gle/3l5i5Mr
+  for (const component of place.address_components as google.maps.GeocoderAddressComponent[]) {
+    // @ts-ignore remove once typings fixed
+    const componentType = component.types[0];
+
+    switch (componentType) {
+      case "street_number": {
+        address1 = `${component.long_name} ${address1}`;
+        break;
+      }
+
+      case "route": {
+        address1 += component.short_name;
+        break;
+      }
+
+      case "postal_code": {
+        postcode = `${component.long_name}${postcode}`;
+        break;
+      }
+
+      case "postal_code_suffix": {
+        postcode = `${postcode}-${component.long_name}`;
+        break;
+      }
+
+      case "locality":
+        (document.querySelector("#locality") as HTMLInputElement).value =
+          component.long_name;
+        break;
+
+      case "administrative_area_level_1": {
+        (document.querySelector("#state") as HTMLInputElement).value =
+          component.short_name;
+        break;
+      }
+
+      case "country":
+        (document.querySelector("#country") as HTMLInputElement).value =
+          component.long_name;
+        break;
+    }
+  }
+
+  address1Field.value = address1;
+  postalField.value = postcode;
+
+  // After filling the form with address components from the Autocomplete
+  // prediction, set cursor focus on the second address line to encourage
+  // entry of subpremise information such as apartment, unit, or floor number.
+  address2Field.focus();
+}
+
+declare global {
+  interface Window {
+    initAutocomplete: () => void;
+  }
+}
+window.initAutocomplete = initAutocomplete;
+export {};
+
+*/
 
 /*
 curl -i -X POST \
